4048b739681c31dac74f80f11721b8229b22505d
Deepak Aggarwal <46883581+deepakagg123@users.noreply.github.com>
2024-04-10 09:31:27 +0100

    Update job_MPI.sh
    script modified for correct instance

--- lab3/job_MPI.sh
+++ lab3/job_MPI.sh
@@ -3,10 +3,10 @@
 #SBATCH --output=lab3_run1_%A.out
 #SBATCH --error=lab3_run1_%A.err
 #SBATCH --account=CA-CS1HGN-015
-#SBATCH --qos=paygo-ncv3-6
-#SBATCH --nodes=2
-#SBATCH --ntasks-per-node=6
-#SBATCH --partition=paygo-ncv3-6
+#SBATCH --qos=paygo-fsv2-2
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --partition=paygo-fsv2-2
 #SBATCH --time=00:01:00
 
 # Load the module for compiling the code
@@ -14,10 +14,9 @@ module load mpi/openmpi
 
 # Print the jobid and node on which it is running
 echo "Running task ${SLURM_JOB_ID} on nodes ${SLURMD_NODENAME}" >> test3.txt
-echo "Running task ${SLURM_JOB_ID} on nodes $(hostname)" >> test3.txt
 
 # Compile the code
 mpicc -o hello_mpi_C hello_mpi.c
 
 # Run the sequential code
-mpirun -np 12 ./hello_mpi_C >> test3.txt
+mpirun -np 2 ./hello_mpi_C >> test3.txt
